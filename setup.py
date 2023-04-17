2ae7ba117d34564e3f0de6284a45fa90d0254006
pedevineau <pierre-etienne.devineau@data.gouv.fr>
2023-04-17 11:10:17 +0200

    Add structure of repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Finetuning open source LLMs with data of French public service',
+    author='pedevineau @ Etalab / DINUM',
+    license='MIT',
+)
