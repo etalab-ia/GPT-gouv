16de4b25ac5eb3da349185480b9409fc28c26e8a
pedevineau <pierre-etienne.devineau@data.gouv.fr>
2023-05-22 19:29:34 +0200

    Change the name of chroma data collection

--- src/data/search_chromadb.py
+++ src/data/search_chromadb.py
@@ -2,20 +2,26 @@ import chromadb
 import pandas as pd
 from chromadb.utils import embedding_functions
 from chromadb.config import Settings
+import os
+
 chroma_client = chromadb.Client(
     Settings(
         chroma_db_impl="duckdb+parquet",
-        persist_directory="../../data/processed" # Optional, defaults to .chromadb/ in the current directory
+        persist_dir = os.path.join("..", "..", "data", "processed")
     )
 )
 
-
-sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
-collection = chroma_client.get_or_create_collection(name="my_collection", embedding_function=sentence_transformer_ef)
+model_name = "all-MiniLM-L6-v2"
+sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction(
+    model_name=model_name
+)
+collection = chroma_client.get_or_create_collection(
+    name=model_name, embedding_function=sentence_transformer_ef
+)
 
 
 results = collection.query(
-    query_texts="Bonjour je paye trop d'impôts",
-    n_results=5
+    query_texts="Bonjour comment puis-je obtenir une nouvelle carte d'identité ?",
+    n_results=5,
 )
 print(results)
--- src/data/store_chromadb.py
+++ src/data/store_chromadb.py
@@ -2,31 +2,36 @@ import chromadb
 import pandas as pd
 from chromadb.utils import embedding_functions
 from chromadb.config import Settings
+import os
+
 chroma_client = chromadb.Client(
     Settings(
         chroma_db_impl="duckdb+parquet",
-        persist_directory="../../data/processed" # Optional, defaults to .chromadb/ in the current directory
+        persist_dir = os.path.join("..", "..", "data", "processed")
     )
 )
 
+model_name = "all-MiniLM-L6-v2"
 
+sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction(
+    model_name=model_name
+)
+collection = chroma_client.get_or_create_collection(
+    name=model_name, embedding_function=sentence_transformer_ef
+)
 
-sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
-collection = chroma_client.get_or_create_collection(name="my_collection", embedding_function=sentence_transformer_ef)
-
-answers = pd.read_csv("../../data/processed/processed_answers_spp.csv", sep="~")[["titre", "expérience", "typologie", "processed_answers"]]
+answers = pd.read_csv("../../data/processed/processed_answers_spp.csv", sep="~")[
+    ["titre", "expérience", "typologie", "processed_answers"]
+]
 answers["question"] = answers["titre"] + " " + answers["expérience"]
 
 n = -1
 ### ADD EMBEDDINGS
 collection.add(
     documents=list(answers["question"].values[:n]),
-    metadatas=[{"answer": answer, "typology": answers["typologie"].values[k]} for k, answer in enumerate(answers["processed_answers"].values[:n])],
-    ids=[str(id) for id in answers.index[:n].values]
+    metadatas=[
+        {"answer": answer, "typology": answers["typologie"].values[k], "title": answers["titre"].values[k]}
+        for k, answer in enumerate(answers["processed_answers"].values[:n])
+    ],
+    ids=[str(id) for id in answers.index[:n].values],
 )
-
-results = collection.query(
-    query_texts=answers["question"].values[n],
-    n_results=5
-)
-print(results)
